8eff4b5602e2f5b0daf2eed98373a8266068f1b6
Deep6908 <deepoffi77@gmail.com>
2026-02-18 20:57:48 +0530

    add user and video model

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -8,14 +8,14 @@ class ApiError extends Error {  // ApiError class inheriting features from paren
         super(message)  // super calls the parent Error class constructor
         this.statusCode = statusCode
         this.data = null    // placeholder for extra data (not used yet), its like a future storage box
-        this.message = message
-        this.success = false
-        this.errors = errors
+        this.message = message // stores the error message
+        this.success = false   // indicates that the API request failed
+        this.errors = errors   // stores the array of detailed errors
 
-        if(stack) {
+        if(stack) {    // if custom stack is provided, use the provided stack trace
             this.stack = stack
         } else {
-            Error.captureStackTrace(this, this.constructor)
+            Error.captureStackTrace(this, this.constructor) // otherwise automatically generate a stack trace showing where the error occurred
         }
     }
 }
